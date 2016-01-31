3a8c9482428ae657880483f84ded0b24888ed85d
Zhaeong <owen_zzz@hotmail.com>
2016-01-30 21:45:10 -0500

    added teleboundary and moving platforms

--- Assets/Scripts/TeleportationController.cs
+++ Assets/Scripts/TeleportationController.cs
@@ -1,28 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TeleportationController : MonoBehaviour {
     public GameObject PlayerObj;
     public GameObject TeleportationObj;
+
+    public Text rangeText;
     public float fMovementRange = 10;
     public float fSpeed = 4.5f;
     private float fSpacing = 1.0f;
     private Vector3 vPos;
+    private Vector3 vDistance;
 
 
     // Use this for initialization
     void Start () {
-        //vPos = TeleportationObj.transform.position;
-        //vPos = new Vector3(0, 0, 0);
+        InvokeRepeating("GetRange", 1, 1);
+
+        rangeText.text = "";
+
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
+        Vector3 vDistance = TeleportationObj.transform.position - PlayerObj.transform.position;
+        
+        Vector3 vClampDist = vDistance;
+        vClampDist.x = Mathf.Clamp(vClampDist.x , PlayerObj.transform.position.x - 2.2f, PlayerObj.transform.position.x + 2.2f);
+        vClampDist.z = Mathf.Clamp(vClampDist.z , PlayerObj.transform.position.z - 2.2f, PlayerObj.transform.position.z + 2.2f);
+        //transform.position = vClampDist;
+        if (vDistance.magnitude > 3)
+        {
+            TeleportationObj.transform.position = vClampDist;
+        }
+        
 
-        //Vector3 vDistance = TeleportationObj.transform.position - PlayerObj.transform.position;
         Debug.DrawLine(PlayerObj.transform.position, TeleportationObj.transform.position, new Color(1, 0 ,0));
-        //Debug.Log(vDistance.sqrMagnitude);
+        //Debug.Log(vDistance.magnitude);
         //if (vDistance.sqrMagnitude < fMovementRange)
         //{
             vPos = new Vector3(0, 0, 0);
@@ -43,9 +60,12 @@ public class TeleportationController : MonoBehaviour {
             {
                 vPos.x += fSpacing;
             }
-
+            if (vDistance.magnitude > 5)
+            {
+                
+            }
             TeleportationObj.transform.Translate(vPos * fSpeed * Time.deltaTime);
-        //}
+      
 
         //Teleport player to object
         if (Input.GetKey("e"))
@@ -54,5 +74,12 @@ public class TeleportationController : MonoBehaviour {
             PlayerObj.transform.position = vTeleLocation;
         }
 
+    }
+
+
+    void GetRange()
+    {
+        vDistance = TeleportationObj.transform.position - PlayerObj.transform.position;
+        rangeText.text = "Range to Player: " + vDistance.sqrMagnitude.ToString();
     }
 }
