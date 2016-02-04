8f5f8c6726747ae21201f774aff64d7a7e70fe75
Simon Joon Hee Song <joonhee.song@gmail.com>
2016-02-04 16:33:58 -0500

    Magnification platform, you cannot teleport out of the mag platform with radius changed back to normal

--- Assets/Scripts/DisableTeleport.cs
+++ Assets/Scripts/DisableTeleport.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class DisableTeleport : MonoBehaviour {
-	public bool charHere;
 	void OnTriggerEnter (Collider col)
 	{
 		col.transform.Find ("TeleTarget").GetComponent<TeleportationController>().canTeleport = false;
