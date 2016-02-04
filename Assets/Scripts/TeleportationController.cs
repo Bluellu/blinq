b52dc960583995030cdd7f8b3a1bc0b29aac904d
Zhaeong <owen_zzz@hotmail.com>
2016-02-04 12:02:52 -0500

    added some more lerping and platform changes

--- Assets/Scripts/TeleportationController.cs
+++ Assets/Scripts/TeleportationController.cs
@@ -3,41 +3,36 @@ using System.Collections;
 
 public class TeleportationController : MonoBehaviour {
     public GameObject PlayerObj;
+    MeshRenderer modelRen;
 
     public bool canPlayerControl;
 
     public int nState;
     private Vector3 vPos;
     private Vector3 axis = Vector3.up;
-    private Vector3 vPlayerOrigin, vMarkerPosition, vMarkerDirection;
+    private Vector3 vPlayerOrigin, vMarkerPosition, vMarkerDirection, vSavedDestination, vPlayerOriginEnd, vPlayerOriginStart;
     public float fRotationSpeed = 2.0f;
 
     private GameObject playerObject;
     private PlayerController playerController;
-    
-    public float speed = 1.0F;
-    private float startTime;
-    private float journeyLength;
-    private float fMarkerDistance;
+  
 
-    private float fLerpingValue = 0.0f;
+    private float fLerpingValue;
+    
 
     // Use this for initialization
     void Start () {
-        startTime = Time.time;
-        journeyLength = 50.0f;
         playerObject = GameObject.FindGameObjectWithTag("Player");
         playerController = playerObject.GetComponent<PlayerController>();
         canPlayerControl = true;
+        
         vPlayerOrigin = PlayerObj.transform.position;
 
-        transform.position = vPlayerOrigin + new Vector3(3, 0 ,3);
-        
-
+        transform.position = vPlayerOrigin + new Vector3(4, 0 ,4);
+        modelRen = playerObject.GetComponentInChildren<MeshRenderer>();
+        fLerpingValue = 0.0f;
         nState = 0;
-
-
-
+        
     }
 
 
@@ -72,43 +67,54 @@ public class TeleportationController : MonoBehaviour {
         else if (nState == 1)
         {
             transform.parent = null;
-            if (fLerpingValue < 0.1f)
-            {
-                fLerpingValue += Time.deltaTime * 0.1f;
-                playerObject.transform.position = Vector3.Lerp(vPlayerOrigin, vMarkerPosition, fLerpingValue);
-                Debug.Log(fLerpingValue);
-            }
-            else
-            {
-                playerObject.transform.position = Vector3.Lerp(vPlayerOrigin, vMarkerPosition, 1);
-                transform.parent = PlayerObj.transform;
-                MeshRenderer modelRen = playerObject.GetComponentInChildren<MeshRenderer>();
-                modelRen.enabled = true;
-                transform.position = vPlayerOrigin + vMarkerDirection;
 
+            if (LerpingTranslate(vPlayerOriginEnd, vMarkerPosition, playerObject))
+            {
+                vSavedDestination = vPlayerOriginStart + vMarkerDirection;
+                vPlayerOriginEnd = vMarkerPosition;
+                nState = 2;
+            }
+        }
+        //Move marker to saved angle location
+        else if (nState == 2)
+        {
+            if (LerpingTranslate(vPlayerOriginEnd, vSavedDestination, transform.root.gameObject))
+            {
                 nState = 0;
-                fLerpingValue = 0.0f;
+                transform.parent = PlayerObj.transform;
+                modelRen.enabled = true;
                 canPlayerControl = true;
                 playerController.canPlayerControl = true;
-                
             }
-
         }
-
     }
 
     void ResetMandala() {
         canPlayerControl = false;
         playerController.canPlayerControl = false;
-
         vMarkerDirection = transform.position - playerObject.transform.position;
-        fMarkerDistance = Vector3.Distance(vMarkerDirection, playerObject.transform.position);
-        MeshRenderer modelRen = playerObject.GetComponentInChildren<MeshRenderer>();
-        
         modelRen.enabled = false;
         vMarkerPosition = transform.position;
+        vPlayerOriginEnd = PlayerObj.transform.position;
+        vPlayerOriginStart = transform.position;
         nState = 1;
 
+    }
+
+    bool LerpingTranslate(Vector3 vStart, Vector3 vEnd, GameObject goToMove) {
+        Debug.Log(fLerpingValue);
+        if (fLerpingValue < 1.0f)
+        {
+            fLerpingValue += Time.deltaTime * 2.4f;
+            goToMove.transform.position = Vector3.Lerp(vStart, vEnd, fLerpingValue);
+        }
+        else
+        {
+            goToMove.transform.position = Vector3.Lerp(vStart, vEnd, 1);
+            fLerpingValue = 0.0f;
+            return true;
+        }
+        return false;
     }
 
 }
