0c3f1de82204b2c9d300d0f4564d1f4fb70699cf
Zhaeong <owen_zzz@hotmail.com>
2016-02-05 23:58:33 -0500

    some minor syncing stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,7 +39,12 @@ public class PlayerController : MonoBehaviour
     }
 
 
-	void OnCollisionEnter(Collision col) {
-		Debug.Log ("Collided with " + col.gameObject.name);
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ActivationBoundary")
+        {
+            Debug.Log("Entered");
+        }
+
+    }
 }
