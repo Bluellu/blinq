f2e790cd7de7778f5ef153c1b1c27bf9064efc88
Bluellu <blue_l20@hotmail.com>
2016-02-25 11:18:07 -0500

    New monsters and falling platform code. Needs debugging.

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+    private Transform platform;
+    
+	// Use this for initialization
+	void Start () {
+        platform = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // If player touches platform, start falling routine.
+    void OnTriggerEnter(Collider obj)  {
+        if (obj.gameObject.name == "Player")  {
+            Debug.Log("COLLISION");
+            StartCoroutine(falling());    
+        }
+    }
+
+    /* Drop platform after a few seconds */
+    IEnumerator falling() { 
+        yield return new WaitForSeconds(0.8f);
+        platform.position = Vector3.MoveTowards(platform.position, new Vector3(platform.position.x, platform.position.y - 20, platform.position.z), 5 * Time.deltaTime);
+        StartCoroutine(destroyPlatform());
+    }
+
+    IEnumerator destroyPlatform() {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
