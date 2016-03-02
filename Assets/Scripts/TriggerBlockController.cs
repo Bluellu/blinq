0ed218c3098897652c9c0279d346f877b2a79e13
Zhaeong <owen_zzz@hotmail.com>
2016-03-02 04:28:51 -0500

    added trigger blocks

---
+++ Assets/Scripts/TriggerBlockController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBlockController : MonoBehaviour {
+
+    public GameObject objectTotrigger;
+    public GameObject ObjectTomove;
+
+    private bool bMovingDown, bMovingUp;
+    private float fAmount, fIncrement;
+    private TriggeredTileMovement TTM;
+    // Use this for initialization
+    void Start () {
+        bMovingDown = false;
+        bMovingUp = false;
+        fAmount = 0.5f;
+        fIncrement = 0.0f;
+        TTM = ObjectTomove.GetComponent<TriggeredTileMovement>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (bMovingDown)
+        {
+            if (fIncrement < fAmount)
+            {
+                objectTotrigger.transform.Translate(Vector3.down * Time.deltaTime);
+                fIncrement += Time.deltaTime;
+            }
+            else
+                TTM.on = true;
+
+        }
+        else if (bMovingUp)
+        {
+            TTM.on = false;
+            if (fIncrement > 0)
+            {
+                objectTotrigger.transform.Translate(Vector3.up * Time.deltaTime);
+                fIncrement -= Time.deltaTime;
+            }
+            
+                
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            bMovingDown = true;
+            bMovingUp = false;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            bMovingDown = false;
+            bMovingUp = true;
+
+        }
+    }
+}
---
+++ Assets/Scripts/TriggeredTileMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggeredTileMovement : MonoBehaviour {
+
+    public GameObject toObject;
+    private Vector3 origPoint;
+    float distance;
+    public float fSpeed;
+    public bool on;
+    bool reached = false;
+
+    public void Start()
+    {
+        origPoint = transform.position;
+        on = false;
+
+    }
+
+    public void FixedUpdate()
+    {
+        Debug.Log(Vector3.Distance(transform.position, toObject.transform.position));
+        if(on)
+            move(transform.position, toObject.transform.position);
+        else
+            move(transform.position, origPoint);
+
+            
+        
+    }
+
+    void move(Vector3 pos, Vector3 towards)
+    {
+        Vector3 direction = (towards - pos).normalized;
+        if (Vector3.Distance(pos, towards) > 0.5)
+        {
+            transform.Translate(direction * Time.deltaTime * fSpeed, Space.World);
+        }
+
+    }
+}
