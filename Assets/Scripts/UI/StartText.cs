5c8c7f408601f72340cc284441d6393c8da4777b
Bluellu <blue_l20@hotmail.com>
2016-03-22 01:48:05 -0400

    Polished FirstLevel

---
+++ Assets/Scripts/UI/StartText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartText : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("fade");
+    }
+
+
+    IEnumerator fade()
+    {
+
+        yield return new WaitForSeconds(4);
+        //Color.Lerp(tutorialText.color, Color.clear, 10 * Time.deltaTime);
+        GetComponent<Text>().CrossFadeAlpha(0.0f, 2.5f, false);
+        //Destroy(tutorialText);
+    }
+
+}
--- Assets/Scripts/UI/Tutorial.cs
+++ Assets/Scripts/UI/Tutorial.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Tutorial : MonoBehaviour {
+    public Text tutorialText;
+    Color startColour;
+    Color endColour;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+       tutorialText.color = new Color(0, 0, 1, 0.0f);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter (Collider coll) {
+        if (coll.gameObject.tag == "Player")
+        {
+            StartCoroutine("fade");
+        }       	
 	}
+
+    IEnumerator fade()    {
+        tutorialText.color = new Color(0, 0.1f, 0.5f, 1.0f);
+        yield return new WaitForSeconds(8);
+        //Color.Lerp(tutorialText.color, Color.clear, 10 * Time.deltaTime);
+        tutorialText.CrossFadeAlpha(0.0f, 2.5f, false);
+        //Destroy(tutorialText);
+    }
 }
