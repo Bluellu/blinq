2d56bc145ce272153eb779b29a66cc93c8a9b060
Zhaeong <owen_zzz@hotmail.com>
2016-03-22 00:32:02 -0400

    lastscenetran

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,9 @@ public class CameraController : MonoBehaviour
 
     public GameObject PlayerChar;
     public GameObject EndObj;
-
+    public bool lastScene;
     private LampStagController LSC;
+    private LampController LC;
     public float cameraXval = 0;
     public float cameraYval = 3;
     public float cameraZval = -5;
@@ -21,15 +22,42 @@ public class CameraController : MonoBehaviour
     {
         speed = 4.0f;
         chaseRange = 10.0f;
-
-        LSC = EndObj.GetComponent<LampStagController>();
+        if(lastScene)
+            LSC = EndObj.GetComponent<LampStagController>();
+        else
+            LC = EndObj.GetComponent<LampController>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!LSC.ReachedEnd)
+        if (lastScene)
+        {
+            if (!LSC.ReachedEnd)
+            {
+                Vector3 cameraPosit = transform.position;
+
+                float cameraX = ((PlayerChar.transform.position.x + cameraXval) * .05F) + (transform.position.x * .95F);
+                float cameraY = ((PlayerChar.transform.position.y + cameraYval) * .05F) + (transform.position.y * .95F);
+                float cameraZ = ((PlayerChar.transform.position.z + cameraZval) * .05F) + (transform.position.z * .95F);
+
+                transform.position = new Vector3(cameraX, cameraY, cameraZ);
+                cameraPosit = new Vector3(cameraX - cameraXval, cameraY - cameraYval, cameraZ - cameraZval);
+                transform.LookAt(cameraPosit);
+            }
+            else
+            {
+                range = Vector3.Distance(transform.position, EndObj.transform.position);
+                //if (range >= chaseRange)
+                //{
+
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            }
+        }
+        else
+        if (!LC.ReachedEnd)
         {
             Vector3 cameraPosit = transform.position;
 
@@ -52,6 +80,7 @@ public class CameraController : MonoBehaviour
         }
 
 
-	}
+
+    }
 
 }
