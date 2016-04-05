463f40abcb252b7ac8a6ce4da50975f80b29b845
rosalindmc <reid.m.chapman@gmail.com>
2016-04-05 18:45:51 -0400

    tutorial Update

--- Assets/Scripts/UI/Tutorial.cs
+++ Assets/Scripts/UI/Tutorial.cs
@@ -9,7 +9,7 @@ public class Tutorial : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-       tutorialText.color = new Color(0, 0, 1, 0.0f);
+       tutorialText.color = new Color(1, 1, 1, 0.0f);
     }
 	
 	// Update is called once per frame
@@ -21,8 +21,8 @@ public class Tutorial : MonoBehaviour {
 	}
 
     IEnumerator fade()    {
-        tutorialText.color = new Color(1f, 0.1f, 0.3f, 1.0f);
-        yield return new WaitForSeconds(8);
+        tutorialText.color = new Color(1f, 1f, 1f, 1.0f);
+        yield return new WaitForSeconds(4);
         //Color.Lerp(tutorialText.color, Color.clear, 10 * Time.deltaTime);
         tutorialText.CrossFadeAlpha(0.0f, 2.5f, false);
         //Destroy(tutorialText);
